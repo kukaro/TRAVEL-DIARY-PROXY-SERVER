80326e2727e512847997a07626ea9774c592d35a
kukaro <justkukaro@naver.com>
2020-07-11 03:15:22 +0900

    스크립트 수정

--- script/sh/init.sh
+++ script/sh/init.sh
@@ -1,2 +1,2 @@
 docker build -t travel-diary-proxy-server-project ../../project/
-docker-compose -f ../../project/docker-compose.yml up -d
+HOST_IP=10.12.204.27 docker-compose -f ../../project/docker-compose.yml up -d
